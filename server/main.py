c7be37d8f94fe71e7e754fa1902876abb8f14aee
charli <charli.vannood@gmail.com>
2024-11-11 20:26:28 +0100

    the start of streaming

--- server/main.py
+++ server/main.py
@@ -3,28 +3,30 @@ import numpy as np
 import asyncio
 import websockets
 import json
+import time
+from PIL import Image
 
-async def send_periodic_messages(websocket):
-    while True:
-        message = json.dumps({"type": "frame", "data": "0x0x0"})
-        try:
-            await websocket.send(message)
-        except websockets.exceptions.ConnectionClosed:
-            print("Client disconnected")
-            break
-        await asyncio.sleep(0.0166)
+async def sendFrame(websocket):
+    startTime = time.time()
+    img_array = await screenshot_to_array()
+    #image = str(img_array.reshape(-1, 4).tolist()).replace(", ", "x").replace("x255]", "]")
+    print("finished screenshot in " + str((time.time() - startTime) * 1000) + "ms")
+    image = await compress(img_array)
+    print("finished image in " + str((time.time() - startTime) * 1000) + "ms")
+
+    message = json.dumps({"type": "frame", "data": str(image)})
+    await websocket.send(message)
 
 async def handle_client(websocket, path):
     print(f"Client connected from {path}")
-    asyncio.create_task(send_periodic_messages(websocket))
-    
     try:
         async for message in websocket:
             print(message)
             data = json.loads(message)
-            print(data)
             if data["type"] == "connect":
                 print(f"connecting to game: {data['game']}")
+            elif data["type"] == "getFrame":
+                await sendFrame(websocket)
     except websockets.exceptions.ConnectionClosed:
         print("Client disconnected")
 
@@ -33,16 +35,53 @@ async def start_server():
     print("WebSocket server is running on ws://localhost:8765")
     await server.wait_closed()
 
-def screenshot_to_array():
+async def screenshot_to_array():
     with mss.mss() as sct:
-        screenshot = sct.grab(sct.monitors[0])
+        screenshot = sct.grab(sct.monitors[1])
         img_array = np.array(screenshot)
-        print(f"Screenshot shape: {img_array.shape}")
+        img = Image.fromarray(img_array)
+        img_resized = img.resize((1000, 500))
+        img_array = np.array(img_resized)
         return img_array
 
-img_array = screenshot_to_array()
+async def colorExists(colors, color):
+    try:
+        a = colors[color]
+        return a
+    except:
+        return False
 
-pixel_color = img_array[100, 100]
-print(f"Pixel color at (100, 100): {pixel_color}")
+async def compress(image):
+    colors = {}
+    pixels = []
+    previousColor = -1
+    colorSequenceLength = 0
+    currentColor = -1
+
+    image = image.reshape(-1, 4).tolist()
+
+    for pixel in image:
+        pixel = f"{round(pixel[0] / 15) * 15}x{round(pixel[1] / 15) * 15}x{round(pixel[2] / 15) * 15}"
+        doesColorExist = await colorExists(colors, pixel)
+        if doesColorExist == False:
+            colors[pixel] = len(colors)
+            currentColor = len(colors) - 1
+        else:
+            currentColor = doesColorExist
+        
+        if previousColor != currentColor:
+            pixels.append(f"{previousColor}x{colorSequenceLength}_")
+            previousColor = currentColor
+            colorSequenceLength = 1
+        else:
+            colorSequenceLength += 1
+    
+    if previousColor != -1:
+        pixels.append(f"{previousColor}x{colorSequenceLength}_")
+
+    colors_ = "_".join(colors)
+    pixels_ = "".join(pixels)
+
+    return colors_ + "#" + pixels_
 
 asyncio.run(start_server())
--- web/streamingJS/main.js
+++ web/streamingJS/main.js
@@ -1,15 +1,30 @@
-console.log("starting renderer")
-
+const canvas = document.getElementById("gameWindow")
+const ctx = canvas.getContext("2d")
 const socket = new WebSocket("ws://localhost:8765");
+
+const gameResolution = [1000, 500]
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+function requestFrames() {
+    socket.send(JSON.stringify({"type": "getFrame"}));
+}
 
 socket.onopen = function() {
     console.log("Connected to WebSocket server");
 
     socket.send(JSON.stringify({"type": "connect", "game": 2}));
+
+    setInterval(requestFrames, 20)
 };
 
 socket.onmessage = function(event) {
-    console.log(event.data)
+    data = JSON.parse(event.data)
+    console.log(data)
+    if (data["type"] == "frame") {
+        drawFrameDecompres(data["data"])
+    }
 };
 
 socket.onclose = function() {
@@ -19,3 +34,50 @@ socket.onclose = function() {
 socket.onerror = function(error) {
     console.error("WebSocket Error:", error);
 };
+
+function drawFrame(imageData) {
+    data = JSON.parse(imageData.replace(/[x]/g, ", "))
+    x = 0
+    y = 0
+
+    pixelWidthX = canvas.width / gameResolution[0]
+    pixelWidthY = canvas.height / gameResolution[1]
+
+    for (let i = 0; i < data.length; i++) {
+        ctx.fillStyle = "rgb(" + data[i][2] + ", " + data[i][1] + ", " + data[i][0] + ")"
+        ctx.fillRect(x * pixelWidthX, y * pixelWidthY, pixelWidthX + 1, pixelWidthY + 1)
+        x += 1
+        if (x >= gameResolution[0]) {
+            y += 1
+            x = 0
+        }
+    }
+}
+
+
+function drawFrameDecompres(imageData) {
+    data = imageData.split("#")
+    colors = data[0].split("_")
+    pixels = data[1].split("_")
+    x = 0
+    y = 0
+
+    pixelWidthX = canvas.width / gameResolution[0]
+    pixelWidthY = canvas.height / gameResolution[1]
+
+    for (let i = 0; i < pixels.length; i++) {
+        if (pixels[i] == "") continue
+        pixelData = pixels[i].split("x")
+        if (pixelData[0] == "-1") continue
+        color = colors[Number(pixelData[0])].split("x")
+        for (j = 0; j < Number(pixelData[1]); j++) {
+            ctx.fillStyle = "rgb(" + color[2] + ", " + color[1] + ", " + color[0] + ")"
+            ctx.fillRect(x * pixelWidthX, y * pixelWidthY, pixelWidthX + 1, pixelWidthY + 1)
+            x += 1
+            if (x >= gameResolution[0]) {
+                y += 1
+                x = 0
+            }
+        }
+    }
+}
