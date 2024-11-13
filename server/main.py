2a8b5c370b074688b4e5157640ceafb50d63aa03
charli <123391187+CharliVanNood@users.noreply.github.com>
2024-11-13 16:54:37 +0100

    data loading from steam file

--- server/main.py
+++ server/main.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, jsonify, render_template_string
+import json
 
 from ConnectToPI import receive_data_rpi
+
+with open("data/steam.json", "r") as f:
+    steamData = json.loads(f.read())
+
+print(steamData[0])
 
 app = Flask(__name__)
 
@@ -11,7 +17,8 @@ def receive_data():
 @app.route('/getGameData')
 def getGameData():
     game_id = request.args.get('id')
-    print(game_id)
-    return jsonify({"success": 200})
-
+    if not game_id.isnumeric(): return jsonify({"success": 404, "data": "no id has been given"})
+    if int(game_id) < len(steamData): return jsonify({"success": 200, "data": steamData[int(game_id)]})
+    return jsonify({"success": 404, "data": "id is out of bounds"})
+    
 app.run(host='0.0.0.0', port=8080)
